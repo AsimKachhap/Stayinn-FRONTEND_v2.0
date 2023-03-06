038959caa85b6a9fc02c0af9c2eae61a7a942122
unknown <asim26k@gmail.com>
2023-03-06 23:55:04 +0530

    stated making layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Footer from "../components/Footer";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <section className="Topbar ">Topbar</section>
+      <div className="flex flex-row gap-2 ">
+        <div>Hotel Cards</div>
+        <div>Map</div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
