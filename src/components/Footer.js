f20fd0fe00db95d53f1f29e0d29092cc683f6c59
unknown <asim26k@gmail.com>
2023-03-08 01:11:47 +0530

    completed the Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="absolute bottom-0">Footer</div>;
+  return (
+    <div className="absolute bottom-0 w-full px-10 py-4 flex flex-row border-t-[1px] border-neutral-400 justify-between ">
+      <div>
+        <span> &#169;</span> 2023 Stayinn Inc
+      </div>
+      <div>
+        This Site is developed by <span>Asim Kachhap</span>.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
