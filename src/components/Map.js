bfba67f0d1214a8dc30da319dc93e11ea01faba0
unknown <asim26k@gmail.com>
2023-03-09 04:22:44 +0530

    Now you can display custom Markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { MapContainer, TileLayer, UseMap, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
 
 const Map = () => {
+  const redMarker = new Icon({
+    iconUrl: "/assets/images/location-pin-red.png",
+
+    iconSize: [30, 30],
+  });
   return (
     <MapContainer
       center={[28.6304, 77.2177]}
@@ -14,7 +20,7 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[28.6304, 77.2177]}>
+      <Marker position={[28.6304, 77.2177]} icon={redMarker}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
