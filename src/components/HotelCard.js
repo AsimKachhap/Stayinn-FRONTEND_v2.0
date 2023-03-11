cb3f13e8bb078c63ba8c09e55ab974fb2d01d8ba
unknown <asim26k@gmail.com>
2023-03-11 11:24:34 +0530

    Implemented Like/Unlike functionslity on UI Layer.

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 const HotelCard = () => {
   const hotel = {
@@ -10,12 +12,34 @@ const HotelCard = () => {
       "https://sitecore-cd-imgr.shangri-la.com/MediaFiles/2/9/9/%7B299F476C-FF02-4CA9-8DD7-2C7C3BE3CCA0%7DSLND_AboutHotel_Banner_1920x940.jpg?w=600&h=500&mode=crop&scale=both",
   };
 
+  const [isLiked, setIsLiked] = useState(false);
+
   return (
     <div className="flex flex-col rounded-md">
       <div className=" object-cover ">
         <img src={hotel.displayImage} alt="hotel" />
       </div>
-      <div></div>
+      <div className="flex flex-row justify-between p-3">
+        <div>1</div>
+
+        <div
+          className={`h-[35px] w-[35px]  z-10 cursor-pointer rounded-md flex items-center justify-center ${
+            isLiked ? "bg-pink-200" : "bg-neutral-200"
+          }`}
+        >
+          {isLiked ? (
+            <AiFillHeart
+              className="text-pink-600 h-[30px] w-[30px] "
+              onClick={() => setIsLiked(false)}
+            />
+          ) : (
+            <AiOutlineHeart
+              className=" text-neutral-600 h-[30px] w-[30px] bg-transparent"
+              onClick={() => setIsLiked(true)}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
