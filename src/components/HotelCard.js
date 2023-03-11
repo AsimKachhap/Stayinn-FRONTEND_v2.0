6fb5e4879d1bd1af9dd510b70be5e704f7d9b1e3
unknown <asim26k@gmail.com>
2023-03-11 07:31:14 +0530

    started working on HotelCard

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 const HotelCard = () => {
+  const hotel = {
+    id: 3857,
+    name: "Shangri La",
+    location: "Janpath New Delhi",
+    startingPrice: 3375,
+    displayImage:
+      "https://sitecore-cd-imgr.shangri-la.com/MediaFiles/2/9/9/%7B299F476C-FF02-4CA9-8DD7-2C7C3BE3CCA0%7DSLND_AboutHotel_Banner_1920x940.jpg?w=600&h=500&mode=crop&scale=both",
+  };
+
   return (
     <div className="flex flex-col rounded-md">
-      <div className="bg-amber-300 min-h-[200px]"></div>
+      <div className=" object-cover "></div>
       <div></div>
     </div>
   );
